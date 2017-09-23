350cb51442db38f99fa45d75b8b0ed32fdf0c4bf
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-23 17:57:20 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,10 +17,9 @@ end
 def monopoly_with_second_tier
 	monopoly = {
 		:railroads => {
-			:pieces => {},
-			:names => {},
-			:rent_in_dollars => {}
-		
+			:pieces => ,
+			:names => ,
+			:rent_in_dollars => 
 		}
 	}
 end
